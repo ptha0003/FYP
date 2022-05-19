95e7ca6d7f3711f1d07944fdfe04bccd0cc6d242
Phi <ptham0998@gmail.com>
2022-05-20 00:47:01 +1000

    removed password from service.js and use variables instead. Added gitignore text file

--- Gateway/WebInterface/service.js
+++ Gateway/WebInterface/service.js
@@ -9,13 +9,18 @@ const path = require('path');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", express.static(path.join(__dirname, "dist/web-interface")));
-mongoose.connect('mongodb://admin:SUPERSECRETPASSWORD@192.168.0.14:27017/RFIDFYP', function (err) {
+var userName = 'XXXX'; //enter your db username
+var pass = 'XXX'; //enter your db pass
+var mongooseConnectionString = `mongodb://${userName}:${pass}@192.168.0.14:27017/RFIDFYP`;
+//connection to mongoDB using mongoose
+mongoose.connect(mongooseConnectionString, function (err) {
     if (err) {
         return console.log('Mongoose - connection error:', err);
     }
     console.log('Connect Successfully');
 });
 
+//web routes
 app.post('/registeredusers', registeredUser.createOne); 
 app.get('/detectedusers',detectedUsers.getAll);
 
